58ade6fae06ca71347593e2f0d3b2848a954fdf6
Rock070 <a0938130702@gmail.com>
2024-08-15 01:53:04 +0800

    refactor: tesing modularize

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,110 +2,105 @@ use axum::{
     body::Body,
     http::{Request, StatusCode},
 };
-
 use shor::Application;
 use tower::ServiceExt;
 
-#[tokio::test]
-async fn test_handle_hash_url() {
-    let app = Application::build()
-        .await
-        .router;
-
-    let response = app
-        .oneshot(
-            Request::builder()
-                .method("POST")
-                .uri("/hash_url")
-                .header("Content-Type", "application/json")
-                .body(Body::from(r#"{"url": "https://google.com"}"#))
-                .unwrap(),
-        )
-        .await
-        .unwrap();
-
-    assert_eq!(response.status(), StatusCode::OK);
+// TODO: 只建立一次app，然後在每個 test 中重複使用
+async fn setup() -> axum::Router {
+    Application::build().await.router
 }
 
-#[tokio::test]
-async fn test_handle_hash_invalid_url() {
-    let app = Application::build()
-        .await
-        .router;
+mod test_hash_url {
+    use super::*;
 
-    let response = app
-        .oneshot(
-            Request::builder()
-                .method("POST")
-                .uri("/hash_url")
-                .header("Content-Type", "application/json")
-                .body(Body::from(r#"{"url": "invalid_url"}"#))
-                .unwrap(),
-        )
-        .await
-        .unwrap();
+    #[tokio::test]
+    async fn test_valid_url() {
+        let app = setup().await;
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method("POST")
+                    .uri("/hash_url")
+                    .header("Content-Type", "application/json")
+                    .body(Body::from(r#"{"url": "https://google.com"}"#))
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
 
-    assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+        assert_eq!(response.status(), StatusCode::OK);
+    }
+
+    #[tokio::test]
+    async fn test_invalid_url() {
+        let app = setup().await;
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method("POST")
+                    .uri("/hash_url")
+                    .header("Content-Type", "application/json")
+                    .body(Body::from(r#"{"url": "invalid_url"}"#))
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+    }
 }
 
+mod test_redirect {
+    use super::*;
 
-#[tokio::test]
-async fn test_handle_redirect_hash_url() {
-    let app = Application::build()
-      .await
-      .router;
+    #[tokio::test]
+    async fn test_valid_hash() {
+        let app = setup().await;
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .uri("/Qhq1TQ2nVI")
+                    .method("GET")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
 
-    let response = app
-        .oneshot(
-            Request::builder()
-                .uri("/Qhq1TQ2nVI")
-                .method("GET")
-                .body(Body::empty())
-                .unwrap(),
-        )
-        .await
-        .unwrap();
+        assert_eq!(response.status(), StatusCode::TEMPORARY_REDIRECT);
+    }
 
-    assert_eq!(response.status(), StatusCode::TEMPORARY_REDIRECT);
-}
+    #[tokio::test]
+    async fn test_invalid_hash() {
+        let app = setup().await;
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .uri("/Qhq1TQ2nVI/")
+                    .method("GET")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
 
-#[tokio::test]
-async fn test_handle_redirect_invalid_hash_url() {
-    let app = Application::build()
-      .await
-      .router;
+        assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+    }
 
-    let response = app
-        .oneshot(
-            Request::builder()
-                .uri("/Qhq1TQ2nVI/")
-                .method("GET")
-                .body(Body::empty())
-                .unwrap(),
-        )
-        .await
-        .unwrap();
+    #[tokio::test]
+    async fn test_nonexistent_hash() {
+        let app = setup().await;
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .uri("/nonexistent")
+                    .method("GET")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
 
-    assert_eq!(response.status(), StatusCode::BAD_REQUEST);
-}
-
-// testing redirect url with valid url, but not exist
-#[tokio::test]
-async fn test_handle_redirect_nonexistent_hash_url() {
-    let app = Application::build()
-        .await
-        .router;
-
-    let response = app
-        .oneshot(
-            Request::builder()
-                .uri("/nonexistent")
-                .method("GET")
-                .body(Body::empty())
-                .unwrap(),
-        )
-        .await
-        .unwrap();
-
-    assert_eq!(response.status(), StatusCode::NOT_FOUND);
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+    }
 }
